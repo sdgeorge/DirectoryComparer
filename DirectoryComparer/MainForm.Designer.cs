f744233e07f6e8ecb4941b14a60810d859a07f0c
Steven <sdgeorge.one@gmail.com>
2020-11-09 11:35:33 +0000

    project creation

---
+++ DirectoryComparer/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DirectoryComparer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DirectoriesLabel = new System.Windows.Forms.Label();
+            this.DirectoriesTextBox = new System.Windows.Forms.TextBox();
+            this.MainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.MainDataGridView = new System.Windows.Forms.DataGridView();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DirectoriesLabel
+            // 
+            this.DirectoriesLabel.AutoSize = true;
+            this.DirectoriesLabel.Location = new System.Drawing.Point(12, 9);
+            this.DirectoriesLabel.Name = "DirectoriesLabel";
+            this.DirectoriesLabel.Size = new System.Drawing.Size(174, 20);
+            this.DirectoriesLabel.TabIndex = 0;
+            this.DirectoriesLabel.Text = "Directories (one per line)";
+            // 
+            // DirectoriesTextBox
+            // 
+            this.DirectoriesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DirectoriesTextBox.Location = new System.Drawing.Point(12, 32);
+            this.DirectoriesTextBox.Multiline = true;
+            this.DirectoriesTextBox.Name = "DirectoriesTextBox";
+            this.DirectoriesTextBox.Size = new System.Drawing.Size(358, 100);
+            this.DirectoriesTextBox.TabIndex = 1;
+            // 
+            // MainProgressBar
+            // 
+            this.MainProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainProgressBar.Location = new System.Drawing.Point(12, 412);
+            this.MainProgressBar.Name = "MainProgressBar";
+            this.MainProgressBar.Size = new System.Drawing.Size(458, 29);
+            this.MainProgressBar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Files";
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(376, 32);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(94, 29);
+            this.BrowseButton.TabIndex = 4;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // MainDataGridView
+            // 
+            this.MainDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainDataGridView.Location = new System.Drawing.Point(12, 167);
+            this.MainDataGridView.Name = "MainDataGridView";
+            this.MainDataGridView.RowHeadersWidth = 51;
+            this.MainDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MainDataGridView.Size = new System.Drawing.Size(458, 239);
+            this.MainDataGridView.TabIndex = 5;
+            this.MainDataGridView.Text = "dataGridView1";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartButton.Location = new System.Drawing.Point(376, 103);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(94, 29);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Analyse";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.MainDataGridView);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MainProgressBar);
+            this.Controls.Add(this.DirectoriesTextBox);
+            this.Controls.Add(this.DirectoriesLabel);
+            this.Name = "MainForm";
+            this.Text = "Directory Comparer";
+            ((System.ComponentModel.ISupportInitialize)(this.MainDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DirectoriesLabel;
+        private System.Windows.Forms.TextBox DirectoriesTextBox;
+        private System.Windows.Forms.ProgressBar MainProgressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.DataGridView MainDataGridView;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
