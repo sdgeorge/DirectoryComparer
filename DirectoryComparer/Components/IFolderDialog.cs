049d3374a514281db22c3d635403c07e59ce2148
Steven <sdgeorge.one@gmail.com>
2020-11-10 00:13:19 +0000

    feat: folder dialog in the style of file dialog

---
+++ DirectoryComparer/Components/IFolderDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace DirectoryComparer.Components
+{
+    // https://www.codeproject.com/Articles/5255769/Csharp-Select-FolderDialog-for-NET-Core-3-0
+    public interface IFolderDialog
+    {
+        string InitialFolder { get; set; }
+        string DefaultFolder { get; set; }
+        string Folder { get; set; }
+        DialogResult ShowDialog();
+        DialogResult ShowDialog(IWin32Window owner);
+        DialogResult ShowVistaDialog(IWin32Window owner);
+        DialogResult ShowLegacyDialog(IWin32Window owner);
+        void Dispose();
+    }
+}
