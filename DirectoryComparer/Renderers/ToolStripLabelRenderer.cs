230ec6a78198a991844b38088a9bba61fc2204f6
Steven <sdgeorge.one@gmail.com>
2020-11-10 00:13:45 +0000

    feat: rendered for tool strip label so that text shows when window is too small to display all

---
+++ DirectoryComparer/Renderers/ToolStripLabelRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DirectoryComparer.Renderers
+{
+    public class ToolStripLabelRenderer : ToolStripProfessionalRenderer
+    {
+        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
+        {
+            if (e.Item is ToolStripStatusLabel)
+                TextRenderer.DrawText(e.Graphics, e.Text, e.TextFont,
+                    e.TextRectangle, e.TextColor, Color.Transparent,
+                    e.TextFormat | TextFormatFlags.EndEllipsis);
+            else
+                base.OnRenderItemText(e);
+        }
+    }
+}
